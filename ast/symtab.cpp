d76708b70e6672eb51bc11f8a73d783583e34259
snowapril <sinjihng@gmail.com>
2021-12-15 00:50:06 +0900

    Implement symbol table (WIP)

--- ast/symtab.cpp
+++ ast/symtab.cpp
@@ -1,1 +1,27 @@
 #include "symtab.hpp"
+#include "node.hpp"
+
+SymbolTable::SymbolTable()
+{
+    
+}
+
+SymbolTable::~SymbolTable()
+{
+    
+}
+
+VarType SymbolTable::getTypeCode(std::string name) const
+{
+    return _nodeTable.find(name) != _nodeTable.end() ? _nodeTable.at(name)->type : VarType::INVALID;
+}
+
+void SymbolTable::addNode(std::string name, VarType type, Node* value)
+{
+
+}
+
+void SymbolTable::addFunction(std::string name, FuncStmt* func)
+{
+    _funcTable.emplace(name, func);
+}
--- ast/symtab.hpp
+++ ast/symtab.hpp
@@ -1,4 +1,28 @@
 #ifndef SYM_TAB_HPP_
 #define SYM_TAB_HPP_
 
+#include <string>
+#include <unordered_map>
+#include "type.hpp"
+
+class Node;
+class FuncStmt;
+
+struct Symbol {
+    std::string id;
+    VarType type;
+    Node* value;
+};
+
+class SymbolTable {
+public:
+    SymbolTable();
+    ~SymbolTable();
+    VarType getTypeCode(std::string name) const;
+    void addNode(std::string name, VarType type, Node* value);
+    void addFunction(std::string name, FuncStmt* func);
+    std::unordered_map<std::string, Symbol*> _nodeTable;
+    std::unordered_map<std::string, FuncStmt*> _funcTable;
+};
+
 #endif // SYM_TAB_HPP_
