004240785f23cf51dc36ff75087662e676f270a3
snowapril <sinjihng@gmail.com>
2021-12-15 00:49:09 +0900

    Implement code generation from symtab

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include "ast/symtab.hpp"
 #include "y.tab.h"
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    yyparse();
+    extern SymbolTable globalSymTab;
+    std::cout << "Hello, Crust!" << std::endl;
     
+    yyparse();
+
+    std::ofstream outfile("output.cpp");
+    for (const auto& func : globalSymTab._funcTable)
+    {
+        auto code = func.second->codeGen(&globalSymTab);
+        for (const auto& line : code)
+        {
+            outfile << line;
+        }
+    }
+    outfile.close();
     return 0;
 }
